0a86d3f114c072bafe29eb823a63c4aba2d3ac29
Pawel Lampe <pawel.lampe@gmail.com>
2020-10-07 20:22:30 +0200

    initial quad w/ small demo

---
+++ source/SugarQuad.gd
@@ -0,0 +1,19 @@
+tool
+extends MeshInstance
+
+enum Effect { NONE, DARKEN }
+
+export (Effect) var effect = Effect.NONE setget _set_effect
+export (Resource) var parameters = null
+
+
+func _set_effect(a_effect):
+	effect = a_effect
+	if effect == Effect.NONE:
+		parameters = null
+		material_override = null
+	if effect == Effect.DARKEN:
+		parameters = ShaderMaterial.new()
+		parameters.shader = preload("res://addons/godot-sugar/source/shaders/3d/darken.shader")
+		material_override = parameters
+	property_list_changed_notify()
---
+++ source/shaders/3d/darken.shader
@@ -0,0 +1,15 @@
+shader_type spatial;
+render_mode unshaded;
+
+uniform float darken_strength : hint_range(0.0, 1.0) = 0.95;
+
+void vertex()
+{
+  POSITION = vec4(VERTEX.xy, -1.0, 1.0);
+}
+
+void fragment()
+{
+  ALBEDO = vec3(0.0);
+  ALPHA = darken_strength;
+}
